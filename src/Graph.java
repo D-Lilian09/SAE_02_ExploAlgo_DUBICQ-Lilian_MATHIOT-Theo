eeb820fa0023a7effda659925ab1421328060b87
Theo Mathiot <theo.mathiot5@etu.univ-lorraine.fr>
2026-05-26 17:25:49 +0200

    ajout de linterface graphe

--- src/Graph.java
+++ src/Graph.java
@@ -1,10 +1,12 @@
-public class Graph {
+public interface Graph {
 
     List<Noeud> LiNoeud = new ArrayList<>();
 
     public Graph() {
         this.LiNoeud = new ArrayList<>();
     }
+    public Noeud recupNoeud();
+    public Arcs recupAdj(Noeud n);
 
 
 
